cc68c0b1d99b816ced0634ebbebbd96e56bca152
kuhnbt <kuhnbt@gmail.com>
2019-01-05 12:19:14 -0500

    changes to admin and models

--- myblog/admin.py
+++ myblog/admin.py
@@ -2,7 +2,19 @@ from django.contrib import admin
 from myblog.models import Post
 from myblog.models import Category
 
+class CategoriesInline(admin.TabularInline):
+	model = Category.post.through
+	fields = ('category',)
 
-# Register your models here.
-admin.site.register(Category)
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    fields = ['title', 'text', 'author', 'published_date']
+    inlines = [CategoriesInline]
+
+class CategoriesAdmin(admin.ModelAdmin):
+	exclude = ['post',]
+
+
+#admin.site.register(AuthorAdmin)
+
+admin.site.register(Category, CategoriesAdmin)
+admin.site.register(Post, PostAdmin)
